54019125b4bf639f7e4b4882b0b2d2f69083b2fc
JungInGyu <112679529+JungInGyu@users.noreply.github.com>
2023-03-29 19:26:55 +0900

    Delete python-1.py

--- python-1.py
+++
@@ -1,7 +0,0 @@
-# 1 화면에 Hello World 문자열을 출력하세요.
-print ("Hello World")
-# 2 화면에 Mary's cosmetics을 출력하세요. (중간에 '가 있음에 주의하세요)
-print("Mary's cosmetics")
-# 3 화면에 아래 문장을 출력하세요. (중간에 "가 있음에 주의하세요.)
-# 신씨가 소리질렀다. "도둑이야".
-print('신씨가 소리질렀다. "도둑이야".')
